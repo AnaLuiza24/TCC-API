d8cf1520ad991efc036f16a2341e56cf82e91be6
Ana Luiza <aandrademateus@gmail.com>
2023-11-16 16:19:23 -0300

    endpoints alterados

--- src/repository/acessoriosRepository.js
+++ src/repository/acessoriosRepository.js
@@ -1,61 +1,30 @@
 import connection from "./connection.js";
 
-export async function ConsultarAcessorios(marca) {
-    let sql = `select nm_produto        as produto, 
-                      vl_preco          as preco, 
-                      ds_cor            as cores, 
-                      nr_quantidade     as qtd, 
-                      ds_produto        as descri, 
-                      vl_preco_promocao as promocao, 
-                      url_imagem_um     as img1, 
-                      url_imagem_dois   as img2, 
-                      id_produto        as id 
-                from tb_produto 
-          inner join tb_categoria on tb_categoria.id_categoria = tb_produto.id_categoria
-               where id_marca = ?
-          `;
-
-    let [dados] = await connection.query(sql, [marca]);
-    return dados;
-}
-
-
-
-
 export async function ListarTodosAcessorios() {
-    let sql = `select nm_produto        as produto, 
-                      vl_preco          as preco, 
-                      ds_cor            as cores, 
-                      nr_quantidade     as qtd, 
-                      ds_produto        as descri, 
-                      vl_preco_promocao as promocao, 
-                      url_imagem_um     as img1, 
-                      url_imagem_dois   as img2, 
-                      id_produto        as id 
-                from tb_produto 
-          inner join tb_categoria on tb_categoria.id_categoria = tb_produto.id_categoria
-  
-          `;
+      let sql =  `select
+                        url_imagem_um     as imagem, 
+                        nm_produto        as nome,
+                        vl_preco          as preco, 
+                        vl_preco_promocao as promocao, 
+                        tb_produto.id_produto 
+                  from tb_produto 
+                  where id_categoria like 1`;
 
     let [dados] = await connection.query(sql);
     return dados;
 }
 
 export async function ProdutoAcessorios(id) {
-      let sql = `select nm_produto        as produto, 
+      let sql =  `select
+                        url_imagem_um     as imagem, 
+                        nm_produto        as nome,
                         vl_preco          as preco, 
-                        ds_cor            as cores, 
-                        nr_quantidade     as qtd, 
-                        ds_produto        as descri, 
                         vl_preco_promocao as promocao, 
-                        url_imagem_um     as img1, 
-                        url_imagem_dois   as img2, 
-                        id_produto        as id 
+                        tb_produto.id_produto 
                   from tb_produto 
-                  inner join tb_categoria on tb_categoria.id_categoria = tb_produto.id_categoria
-                  
-                  `;
+                  where id_categoria like 1
+                        and id_marca = ?`;
 
-      const [linhas] = await connection.query(sql, {id});
+      const [linhas] = await connection.query(sql, [id]);
       return linhas[0];
 }
